43de7de299b745bf5f8c8bb1e163031b3a01c9cf
Gloria Ding <gloria.hc.ding@gmail.com>
2018-08-16 10:37:07 -0700

    Add content to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
-
 export default class Home extends Component {
 
 
   render(){
     return(
       <div className="home-container">
-        <h2>home</h2>
+        <div className="welcome-message">
+          <h1>Hi, I'am</h1>
+          <h1>Gloria Ding</h1>
+        </div>
       </div>
     )
   }
