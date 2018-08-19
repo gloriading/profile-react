f51a165df265ff674ac3dd139c4dde3eb0a867e2
Gloria Ding <gloria.hc.ding@gmail.com>
2018-08-19 13:35:29 -0700

    Add a second title and hover effect to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,10 @@ export default class Home extends Component {
     return(
       <div className="home-container">
         <div className="welcome-message">
+          <p className="job-title-second">Mobile Developer</p>
           <h1>Hi, I'am</h1>
           <h1>Gloria Ding</h1>
-          <p className="job-title">Web Developer</p>
+          <p className="job-title-first">Web Developer</p>
         </div>
       </div>
     )
