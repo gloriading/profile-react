26ebbf92ab2b0086b32437ca689b046c60f3ea2a
Gloria Ding <gloria.hc.ding@gmail.com>
2018-08-28 17:01:43 -0700

    Include project images and create data for project list

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import { projects } from '../data/projects'
 
 
 export default class MyProjects extends Component {
+  constructor(props){
+    super(props)
 
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(){
+    console.log('here');
+    // <h2 onClick={this.handleClick}>my projects</h2>
+  }
 
   render(){
+    let projectList;
+    projectList = projects.map((project, index) => {
+      return <li key={index}>{project.name}</li>
+    })
+
     return(
       <div className="projects-container">
-        <h2>my projects</h2>
+        <h2> myProjects </h2>
+        <div className="inner-container">
+          <div className="project-list">
+            <ul>
+              {projectList}
+            </ul>
+          </div>
+          <div className="project-display">
+
+          </div>
+        </div>
       </div>
     )
   }
---
+++ src/data/projects.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    name: "move",
+    link: "https://healthymove.herokuapp.com/",
+    image: require('../images/project-01.png'),
+    description: "move"
+  },
+  {
+    name: "G Games",
+    link: "https://gloriading.github.io/landing-pages/single-page/product-promo-two.html",
+    image: require('../images/project-02.png'),
+    description: "Product Promotion Page"
+  },
+  {
+    name: "Food Blog",
+    link: "https://gloriading.github.io/landing-pages/single-page/Blog_2/index.html",
+    image: require('../images/project-03.png'),
+    description: "Food Blog"
+  },
+  {
+    name: "Modern Portfolio",
+    link: "https://gloriading.github.io/modern_portfolio/",
+    image: require('../images/project-03.png'),
+    description: "Portfolio Website"
+  }
+]
+
+export {projects}
