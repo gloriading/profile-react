a0f063d762fe229a001b74dfaba015be5d76e8d8
Gloria Ding <gloria.hc.ding@gmail.com>
2018-08-31 14:40:28 -0700

    Fix typo on AboutMe

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -27,7 +27,7 @@ export default class AboutMe extends Component {
           <h2> aboutMe </h2>
 
           <p className="bio">
-            I am a Web developer who loves exploring and learning new technologies from various online sources; moveover, I volunteer as a mentor in several classes with Canada Learning Code, aim to help more peeple discover the fascination of coding. My goal is to become an awesome front-end web & mobile developer!
+            I am a Web developer who loves exploring and learning new technologies from various online sources; moveover, I volunteer as a mentor in several classes with Canada Learning Code, aim to help more people discover the fascination of coding. My goal is to become an awesome front-end web & mobile developer!
           </p>
           <p className="tech">
             Technologies I use: HTML5, CSS, JavaScript, jQuery, Ajax, Bootstrap, React.js, React Native, Apollo, GraphQL, Ruby on Rails, PostgreSQL, GitHub
