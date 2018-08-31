c02c1d6d493f426f3d08ccd19a7025b9915542e3
Gloria Ding <gloria.hc.ding@gmail.com>
2018-08-31 13:05:01 -0700

    Update project descriptions

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,25 +3,25 @@ const projects = [
     name: "move",
     link: "https://healthymove.herokuapp.com/",
     image: require('../images/project-01.png'),
-    description: "move"
+    description: "MOVE is a web app for visualizing exercise records. Users can actually see their achievements and obtain/maintain exercise habits. Built with Ruby on Rails, JavaScript, AJAX, jQuery, HTML5, CSS3, Bootstrap4."
   },
   {
     name: "G Games",
     link: "https://gloriading.github.io/landing-pages/single-page/product-promo-two.html",
     image: require('../images/project-02.png'),
-    description: "Product Promotion Page"
+    description: "Single-page layout with in-page links to ease the navigation. Built with HTML5, CSS3, Bootstrap4, jQuery."
   },
   {
     name: "Food Blog",
     link: "https://gloriading.github.io/landing-pages/single-page/Blog_2/index.html",
     image: require('../images/project-03.png'),
-    description: "Food Blog"
+    description: "Single-page layout with fixed background and scrolling body to engage the audience. Built with HTML5, CSS3."
   },
   {
     name: "Modern Portfolio",
     link: "https://gloriading.github.io/modern_portfolio/",
     image: require('../images/project-04.png'),
-    description: "Portfolio Website"
+    description: "A multi-page portfolio website with modern design. Built with HTML5, SCSS (node-sass)"
   }
 ]
 
