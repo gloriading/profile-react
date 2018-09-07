0bdd2aab762ab4dfda22eeaacb9b20a74178ffed
Gloria Ding <gloria.hc.ding@gmail.com>
2018-09-06 20:28:31 -0700

    Move icons from aboutme to contact

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,24 +1,7 @@
 import React, { Component } from 'react';
-import { FiLinkedin, FiGithub, FiMonitor } from 'react-icons/fi';
-import { Tooltip } from 'reactstrap';
-const Dragon = require('../images/dragon.png');
+
 
 export default class AboutMe extends Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      tooltipOpen: false
-    };
-  }
-
-  toggle() {
-    this.setState({
-      tooltipOpen: !this.state.tooltipOpen
-    });
-  }
-
 
   render(){
     return(
@@ -30,36 +13,10 @@ export default class AboutMe extends Component {
             I am a Web developer who loves exploring and learning new technologies from various online sources; moveover, I volunteer as a mentor in several classes with Canada Learning Code, aim to help more people discover the fascination of coding. My goal is to become an awesome front-end web & mobile developer!
           </p>
           <p className="tech">
-            Technologies I use: HTML5, CSS, JavaScript, jQuery, Ajax, Bootstrap, React.js, React Native, Apollo, GraphQL, Ruby on Rails, PostgreSQL, GitHub
+            Technologies: <br/> HTML5, CSS, JavaScript, jQuery, Ajax, Bootstrap, React.js, React Native, Apollo, GraphQL, Ruby on Rails, PostgreSQL, GitHub
           </p>
 
         </div>
-        <div className="about-image">
-          <div className="img-overlay">
-            <a href="https://www.linkedin.com/in/gloriading/"
-            rel="noopener noreferrer"
-            target="_blank">
-              <FiLinkedin className="icon icon-linkedin" />
-            </a>
-            <a href="https://github.com/gloriading"
-            rel="noopener noreferrer"
-            target="_blank">
-              <FiGithub className="icon icon-github" />
-            </a>
-            <a href="https://gloriading.github.io/profile-examples/gloriousWeb/index.html"
-            rel="noopener noreferrer"
-            target="_blank">
-              <FiMonitor className="icon icon-static-site" id="staticSite"/>
-              <Tooltip
-                placement="top" isOpen={this.state.tooltipOpen}
-                target="staticSite" toggle={this.toggle}>
-                My portfolio site with HTML5 and CSS3
-              </Tooltip>
-            </a>
-          </div>
-          <img src={Dragon} alt="Dragon" className="dragon-img"/>
-        </div>
-
       </div>
     )
   }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
+import { Tooltip } from 'reactstrap';
+import { FiLinkedin, FiGithub, FiMonitor } from 'react-icons/fi';
 
 
 export default class Contact extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      tooltipOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      tooltipOpen: !this.state.tooltipOpen
+    });
+  }
 
 
   render(){
     return(
       <div className="contact-container">
         <h2>contact me</h2>
+        <div className="contact-inner">
+          <a href="https://www.linkedin.com/in/gloriading/"
+          rel="noopener noreferrer"
+          target="_blank">
+            <FiLinkedin className="icon icon-linkedin" />
+          </a>
+          <a href="https://github.com/gloriading"
+          rel="noopener noreferrer"
+          target="_blank">
+            <FiGithub className="icon icon-github" />
+          </a>
+          <a href="https://gloriading.github.io/profile-examples/gloriousWeb/index.html"
+          rel="noopener noreferrer"
+          target="_blank">
+            <FiMonitor className="icon icon-static-site" id="staticSite"/>
+            <Tooltip
+              placement="top" isOpen={this.state.tooltipOpen}
+              target="staticSite" toggle={this.toggle}>
+              My portfolio site with HTML5 and CSS3
+            </Tooltip>
+          </a>
+        </div>
       </div>
     )
   }
