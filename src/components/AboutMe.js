da57b8c49c0410133b57fcd6ba8d43bdbfd76817
Gloria Ding <gloria.hc.ding@gmail.com>
2018-11-03 10:53:37 -0700

    update aboutMe

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,7 +10,8 @@ const AboutMe = () => {
           I am a Web developer who loves exploring and learning new technologies from various online sources; moveover, I volunteer as a mentor in several classes with Canada Learning Code, aim to help more people discover the fascination of coding. My goal is to become an awesome front-end web & mobile developer!
         </p>
         <p className="tech animated fadeInRight slower">
-          Technologies: <br/> HTML5, CSS, JavaScript, jQuery, Ajax, Bootstrap, React.js, React Native, Apollo, GraphQL, Ruby on Rails, PostgreSQL, GitHub
+          <span>Technologies</span>
+          HTML5, CSS, JavaScript, jQuery, Ajax, Bootstrap, React.js, React Native, Apollo, GraphQL, Ruby, Ruby on Rails, PostgreSQL, GitHub, GitLab
         </p>
 
       </div>
